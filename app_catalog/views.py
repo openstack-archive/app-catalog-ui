bad9bf4defa4211d38e1fa7b2c05880cacb1f856
Mark Vanderwiel <vanderwl@us.ibm.com>
2016-02-22 11:56:14 -0600

    Add pep8 and js lint
    Add support for pep8 and js linting
    Will hold off on js cleanups as there are a lot of
    them.  Will make that job non-voting so we can
    make progress.
    
    Change-Id: Iec0c524325a9dca8e1e7b7a9ff599842c4cc3dbf

--- app_catalog/views.py
+++ app_catalog/views.py
@@ -1,12 +1,25 @@
-from horizon import Horizon
-from horizon import views
-from horizon.version import version_info as hvi
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 from app_catalog.version import version_info as acvi
 from django.conf import settings
+from horizon import Horizon
+from horizon.version import version_info as hvi
+from horizon import views
 from openstack_dashboard import api
 import re
 
 import json
+
 
 class IndexView(views.APIView):
     # A very simple class-based view...
@@ -17,23 +30,28 @@ class IndexView(views.APIView):
         try:
             Horizon.get_dashboard('murano')
             has_murano = True
-        except:
+        except Exception:
             pass
         regex = re.compile('(\d+\.\d+\.\d+)-?(.*)')
         heat_version = None
         heat_release = None
         try:
-            info = api.heat.heatclient(request).build_info.build_info()['engine']['revision']
+            info = api.heat.heatclient(
+                request).build_info.build_info()['engine']['revision']
             match = regex.match(info)
             if match:
                 heat_version = match.group(1)
                 heat_release = match.group(0)
-        except:
+        except Exception:
             pass
-        heat_version = getattr(settings, 'APP_CATALOG_HEAT_VERSION', heat_version)
-        heat_release = getattr(settings, 'APP_CATALOG_HEAT_RELEASE', heat_release)
-        murano_version = getattr(settings, 'APP_CATALOG_MURANO_VERSION', None)
-        murano_release = getattr(settings, 'APP_CATALOG_MURANO_RELEASE', None)
+        heat_version = getattr(settings,
+                               'APP_CATALOG_HEAT_VERSION', heat_version)
+        heat_release = getattr(settings,
+                               'APP_CATALOG_HEAT_RELEASE', heat_release)
+        murano_version = getattr(settings,
+                                 'APP_CATALOG_MURANO_VERSION', None)
+        murano_release = getattr(settings,
+                                 'APP_CATALOG_MURANO_RELEASE', None)
         app_catalog_settings = {
             'HEAT_VERSION': {
                 'VER': heat_version,
@@ -52,7 +70,9 @@ class IndexView(views.APIView):
                 'VER': acvi.version_string(),
                 'REL': acvi.release_string()
             },
-            'APP_CATALOG_URL': getattr(settings, 'APP_CATALOG_URL', '//apps.openstack.org')
+            'APP_CATALOG_URL': getattr(settings,
+                                       'APP_CATALOG_URL',
+                                       '//apps.openstack.org')
         }
         context['APP_CATALOG_SETTINGS'] = json.dumps(app_catalog_settings)
         return context
