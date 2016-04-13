7d76fbc9c83f09f83af9347c3d9f8f5f99d4f6e4
Hidekazu Nakamura <hid-nakamura@vf.jp.nec.com>
2016-04-13 15:27:02 +0900

    Fix Devstack and app-catalog-ui interaction
    Devstack always installs test-requirements, but we do not expect it for
    app-catalog-ui. Moreover, such installation started failing with
    PIP versions 8.x
    
    Change-Id: I92be3ee00efd4b94d4a05d09a3a80aa778747e9d
    Closes-Bug: #1569700

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,7 +1,11 @@
 # plugin.sh - DevStack plugin.sh dispatch script app-catalog-ui
 
 function install_app-catalog-ui {
+    mv $APP_CAT_UI_DIR/test-requirements.txt $APP_CAT_UI_DIR/_test-requirements.txt
+
     setup_develop $APP_CAT_UI_DIR
+
+    mv $APP_CAT_UI_DIR/_test-requirements.txt $APP_CAT_UI_DIR/test-requirements.txt
 }
 
 function configure_app-catalog-ui {
